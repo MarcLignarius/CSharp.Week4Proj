3ee00425215e9f6244d3eb1bb0370f22bd967c43
Marc Davies <marcdaviesriot@gmail.com>
2019-05-10 17:35:39 -0700

    begin setup of clients within stylists functionality

---
+++ HairSalon/Controllers/StylistsController.cs
@@ -0,0 +1,46 @@
+using System.Collections.Generic;
+using System;
+using Microsoft.AspNetCore.Mvc;
+using HairSalon.Models;
+
+namespace HairSalon.Controllers
+{
+  public class StylistsController : Controller
+  {
+
+    [HttpGet("/stylists")]
+    public ActionResult Index()
+    {
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View(allStylists);
+    }
+
+    [HttpGet("/stylists/new")]
+    public ActionResult New()
+    {
+      return View();
+    }
+
+    [HttpPost("/stylists")]
+    public ActionResult Create(string name, string description)
+    {
+      Stylist newStylist = new Stylist(name, description);
+      List<Stylist> allStylists = Stylist.GetAll();
+      return View("Index", allStylists);
+    }
+
+    [HttpGet("/stylists/{id}")]
+    public ActionResult Show(int id)
+    {
+        Dictionary<string, object> model = new Dictionary<string, object>();
+        Stylist selectedStylist = Stylist.Find(id);
+        List<Client> stylistClients = selectedStylist.GetClients();
+        model.Add("stylist", selectedStylist);
+        model.Add("clients", stylistClients);
+        return View(model);
+    }
+
+
+
+  }
+}
